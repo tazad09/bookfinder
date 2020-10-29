3492637e93cab9adf82ed82d4b3d5abf6c552942
Tasnim <tasnimazad17@gmail.com>
2020-10-29 01:09:14 -0400

    [When this code is committed it will] Be staged for deployment

--- src/db/database.js
+++ src/db/database.js
@@ -3,6 +3,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "cannot connect to MongoDB"));
+db.once("open", function () {
+  console.log("connected to MongoDB");
+});
 
 const bookSchema = new mongoose.Schema({
   id: String,
