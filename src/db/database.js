f3ea6cec4a6f27bd011d56c81b1facd089983f0a
Tasnim <tasnimazad17@gmail.com>
2020-10-29 00:43:17 -0400

    [When this code is committed it will] Be staged for deployment

--- src/db/database.js
+++ src/db/database.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/books', ()=> {
-  console.log('Connected to mongoose')
-})
+var mongoose = require('mongoose');
+var dbHost = process.env.DB_HOST || '127.0.0.1:27017';
+var dbName = process.env.DB_NAME || 'MVP';
 
+mongoose.connect(`mongodb://${dbHost}/${dbName}`, { useNewUrlParser: true })
+  .then(()=> {console.log('Connected to the db')})
+  .catch(error => handleError(error));
 
 const bookSchema = new mongoose.Schema({
   id: String,
