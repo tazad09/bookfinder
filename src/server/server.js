252245fb02d33d94df50f22e9d2a36f7086f4bf2
Tasnim <tasnimazad17@gmail.com>
2020-10-29 10:07:15 -0400

    [When this code is committed it will] Be staged for deployment

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
+//var dotEnv = require('dotenv').config();
 const PORT = process.env.PORT || 3000
 const db = require('../db/database')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
 
 app.use(express.static("dist"));
 //app.use(express.static(path.join(__dirname + '../../dist')))
