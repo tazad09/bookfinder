664665972014b27fa901ecc323015b24d3451e86
Tasnim <tasnimazad17@gmail.com>
2020-10-29 00:56:36 -0400

    [When this code is committed it will] Be staged for deployment

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const db = process.env.DB_HOST || '127.0.0.1:27017';
-const name = process.env.DB_NAME || 'books'
-
-mongoose.connect(`mongodb://${db}/books`, { useNewUrlParser: true })
-  .then(()=> {console.log('Connected to the db')})
-  .catch(error => handleError(error));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const bookSchema = new mongoose.Schema({
   id: String,
